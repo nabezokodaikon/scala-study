c1e8fb873ca2caaa3b2846cd51f707286c153207
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-12-31 02:14:57 +0900

    テンプレートファイルを削除

--- src/test/scala/com/github/nabezokodaikon/MainTest.scala
+++
@@ -1,9 +0,0 @@
-package com.github.nabezokodaikon
-
-import org.scalatest.FunSuite                       
-
-class MainTest extends FunSuite {                   
-  test("helloWorld") {                              
-    assert(Main.helloWorld("nabezokodaikokn") == "Hello nabezokodaikokn!")
-  }                                                 
-}                                                   
