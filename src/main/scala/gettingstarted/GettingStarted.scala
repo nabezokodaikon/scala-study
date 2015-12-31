aa998da8faeeba5767ef3ab802e4f6f6a63c3d77
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-12-31 23:35:17 +0900

    階乗関数を実装

--- src/main/scala/gettingstarted/GettingStarted.scala
+++ src/main/scala/gettingstarted/GettingStarted.scala
@@ -6,6 +6,14 @@ object MyModule {
     if (n < 0) -n
     else n
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
