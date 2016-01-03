f016e7a91ed160c5ade6c684138e3a2ce7da81ad
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-01-04 01:01:48 +0900

    List を整形

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -7,14 +7,14 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List {
 
   def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
+    case Nil         => 0
     case Cons(x, xs) => x + sum(xs)
   }
 
   def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
+    case Nil          => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x, xs) => x * product(xs)
+    case Cons(x, xs)  => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] =
