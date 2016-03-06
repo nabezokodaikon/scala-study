a9bd201d5932cc04867127f3d851c5e444b85c87
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-06 14:35:26 +0900

    不要関数削除

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,35 +1,14 @@
 package fpinscala.errorhandling
 
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
 object Option {
 
-  def failingFn(i: Int): Int = {
-    val y: Int = throw new Exception("fail!")
-    try {
-      val x = 42 + 5
-      x + y
-    } catch {
-      case e: Exception => 43
-    }
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
   }
 
-  def failingFn2(i: Int): Int = {
-    try {
-      val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int)
-    } catch {
-      case e: Exception => 43
-    }
-  }
-
-  def mean(xs: Seq[Double]): Double = {
-    if (xs.isEmpty)
-      throw new ArithmeticException("mean of empty list!")
-    else xs.sum / xs.length
-  }
-
-  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double = {
-    if (xs.isEmpty)
-      onEmpty
-    else xs.sum / xs.length
-  }
 }
