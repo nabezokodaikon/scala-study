64a3f3e9e2638ac1c33c8e7d37ffb2920cf00e8f
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-06 18:39:49 +0900

    lift 関数を実装
    省略の手順
    1. def lift[A, B](f: A => B): Option[A] => Option[B] = i => i.map(f)
    2. def lift[A, B](f: A => B): Option[A] => Option[B] = _.map(f)

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -59,4 +59,7 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _.map(f)
+
+  def absO: Option[Double] => Option[Double] = lift(math.abs)
 }
