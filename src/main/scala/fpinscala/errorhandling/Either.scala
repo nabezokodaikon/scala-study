0aa65fe29f704aa013468192de40ac6c9e1804b5
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-06 22:27:29 +0900

    サンプルコードを実装

--- src/main/scala/fpinscala/errorhandling/Either.scala
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,3 +1,25 @@
+package fpinscala.errorhandling
+
 sealed trait Either[+E, +A]
 case class Left[+E](value: E) extends Either[E, Nothing]
-case class Left[+A](value: A) extends Either[Nothing, A]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+
+  /** 平均値を取得します。 */
+  def mean(xs: List[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  /** 安全に除算を行います。 */
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  /** スローされた例外を値に変換する関数。 */
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+}
--- src/test/scala/fpinscala/errorhandling/EitherSpec.scala
+++ src/test/scala/fpinscala/errorhandling/EitherSpec.scala
@@ -6,4 +6,23 @@ class EitherSpec extends FlatSpec {
 
   behavior of "Either テスト"
 
+  it should "mean" in {
+    assert(Either.mean(List[Double]()) == Left("mean of empty list!"))
+    assert(Either.mean(List(1.0, 2)) == Right(1.5))
+  }
+
+  it should "safeDiv" in {
+    assert {
+      Either.safeDiv(4, 2) match {
+        case Left(e) => false
+        case Right(v) => true
+      }
+    }
+    assert {
+      Either.safeDiv(4, 0) match {
+        case Left(e) => true
+        case Right(v) => false
+      }
+    }
+  }
 }
