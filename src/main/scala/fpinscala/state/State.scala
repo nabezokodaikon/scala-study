6b9e30b14eab6a4b0e0397596f810dca981bc8d0
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-17 21:40:08 +0900

    nextInt 実装

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -4,3 +4,14 @@ trait RNG {
 
   def nextInt: (Int, RNG)
 }
+
+case class SimpleRNG(seed: Long) extends RNG {
+
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECD66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+}
--- src/test/scala/fpinscala/state/StateSpec.scala
+++ src/test/scala/fpinscala/state/StateSpec.scala
@@ -6,7 +6,11 @@ class StateSpec extends FlatSpec {
 
   behavior of "State テスト"
 
-  it should "Example" in {
-    assert(true)
+  it should "nextInt" in {
+    val rng = SimpleRNG(42)
+    val (n1, rng2) = rng.nextInt
+    val (n2, rng3) = rng2.nextInt
+    assert(n1 == rng.nextInt._1)
+    assert(n2 == rng2.nextInt._1)
   }
 }
