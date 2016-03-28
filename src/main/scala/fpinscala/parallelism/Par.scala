6ca26c19d883320fb12110ef571c7f6edc53346c
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-28 22:23:01 +0900

    EXECIZE 7.6 実装

--- src/main/scala/fpinscala/parallelism/Par.scala
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -105,6 +105,17 @@ object Par {
     sequence(fbs)
   }
 
+  /**
+   * EXECIZE 7.6
+   */
+  def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] = l.map(asyncF((a: A) => {
+      if (f(a)) List(a)
+      else List()
+    }))
+    map(sequence(pars))(_.flatten)
+  }
+
 }
 
 /**
