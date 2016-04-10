400dacfae1efa10cbb23b27f73d69903615694ae
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-10 23:54:40 +0900

    add EXERCIZE 8.4 考え中

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -1,4 +1,28 @@
 package fpinscala.testing
 
-object Gen {
+import fpinscala.state._
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
 }
+
+trait Prop {
+  import Prop._
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+  // def &&(p: Prop): = new Prop {
+  // def check = Prop.this.check && p.check
+  // }
+
+}
+
+// trait Gen[A] {
+
+// def listOf(a: Gen[A]): Gen[List[A]]
+// def listOfN(n: Int, a: Gen[A]): Gen[List[A]]
+// def forAll(a: Gen[A])(f: A => Boolean): Prop
+// }
+
+case class Gen[A](ample: State[RNG, A]) {
+}
+
