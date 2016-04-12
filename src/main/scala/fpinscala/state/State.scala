5394bcf0639558ebbbfa163677e755ea99472ba6
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-12 20:59:58 +0900

    add EXERCISEのテンプレートを作成

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -1,2 +1,69 @@
 package fpinscala.state
 
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  // def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+
+  // def double(rng: RNG): (Double, RNG) = ???
+
+  // def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+
+  // def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+
+  // def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+
+  // def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+
+  // def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+
+  // def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+
+  // def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+}
+
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    sys.error("todo")
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    sys.error("todo")
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    sys.error("todo")
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object State {
+  type Rand[A] = State[RNG, A]
+  // def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
+
--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -36,20 +36,19 @@ object Gen {
    * startからstopExclusiveの範囲内の整数を生成する。
    */
   // def choose(start: Int, stopExclusive: Int): Gen[Int] = {
-    // val rng = SimpleRNG(0)
-    // Gen(State(rng.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+  // val rng = SimpleRNG(0)
+  // Gen(State(rng.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
   // }
 
   /**
    * EXERCIZE 8.5
    */
   // def unit[A](a: => A): Gen[A] =
-    // Gen(State.unit(a))
+  // Gen(State.unit(a))
 
   // def boolean: Gen[Boolean] =
 
   // def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
-
 
 }
 
