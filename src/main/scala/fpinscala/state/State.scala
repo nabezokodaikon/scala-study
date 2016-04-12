98d525e1b2c9999a43f3b66f9d4ba6e6ba2b09f1
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-12 21:35:49 +0900

    add EXERCISE の前まで実装

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -1,18 +1,30 @@
 package fpinscala.state
 
+/**
+ * リスト 6-2
+ */
 trait RNG {
   def nextInt: (Int, RNG)
 }
 
 object RNG {
 
-  case class Simple(seed: Long) extends RNG {
+  /**
+   * リスト 6-3
+   */
+  case class SimpleRNG(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-      val nextRNG = Simple(newSeed)
+      val nextRNG = SimpleRNG(newSeed)
       val n = (newSeed >>> 16).toInt
       (n, nextRNG)
     }
+  }
+
+  def randomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
   }
 
   type Rand[+A] = RNG => (A, RNG)
--- src/test/scala/fpinscala/state/StateSpec.scala
+++ src/test/scala/fpinscala/state/StateSpec.scala
@@ -6,8 +6,16 @@ class StateSpec extends FlatSpec {
 
   behavior of "State テスト"
 
-  it should "example" in {
-    assert(true)
+  it should "randomPair" in {
+    import RNG._
+    val rng = SimpleRNG(42)
+    val ((a1, a2), _) = randomPair(rng)
+    val ((b1, b2), rng2) = randomPair(rng)
+    val ((c1, c2), _) = randomPair(rng2)
+    assert(a1 == b1)
+    assert(a2 == b2)
+    assert(a1 != c1)
+    assert(a2 != c2)
   }
 
 }
