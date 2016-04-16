e1b54c2a768289b734d90ecf598ace756f09af5f
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-17 00:31:34 +0900

    add EXERCISE 6.9

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -167,6 +167,15 @@ object RNG {
         unit(mod)
       else nonNegativeLessThanViaFlatMap(count)
     }
+
+  /**
+   * EXERCISE 6.9
+   */
+  def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => mapViaFlatMap(rb)(b => f(a, b)))
 }
 
 case class State[S, +A](run: S => (A, S)) {
