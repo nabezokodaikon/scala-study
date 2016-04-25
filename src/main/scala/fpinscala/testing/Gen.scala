b48118eb1feb59540b69fa1a319762ea2125cda3
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-25 22:52:14 +0900

    modify EXERCIZE 8.4 実装

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -15,22 +15,36 @@ shell, which you can fill in and modify while working through the chapter.
 
 trait Prop {
 
-  def check: Boolean
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
   // EXERCIZE 8.3
-  def &&(p: Prop): Prop = new Prop {
-    def check = Prop.this.check && p.check
-  }
+  // def &&(p: Prop): Prop = new Prop {
+  // def check = Prop.this.check && p.check
+  // }
 
 }
 
 object Prop {
 
+  type FailedCase = String
+  type SuccessCount = Int
+
   // def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 
 }
 
+case class Gen[A](sample: State[RNG, A])
+
 object Gen {
+
+  // EXERCIZE 8.4
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+
+  def choose2(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(rng => RNG.nonNegativeInt(rng) match {
+      case (n, rng2) => (start + n % (stopExclusive - start), rng2)
+    }))
 
   // def listOf[A](a: Gen[A]): Gen[List[A]]
   // def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]]
@@ -38,10 +52,10 @@ object Gen {
   // def unit[A](a: => A): Gen[A] = ???
 }
 
-trait Gen[A] {
-  // def map[A,B](f: A => B): Gen[B] = ???
-  // def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-}
+// trait Gen[A] {
+// // def map[A,B](f: A => B): Gen[B] = ???
+// // def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+// }
 
 trait SGen[+A] {
 
--- src/test/scala/fpinscala/testing/GenSpec.scala
+++ src/test/scala/fpinscala/testing/GenSpec.scala
@@ -6,9 +6,26 @@ class GenSpec extends FlatSpec {
 
   behavior of "Gen テスト"
 
-  it should "example" in {
-    assert(true)
+  it should "EXERCIZE 8.4 choose" in {
+    import fpinscala.state._
+    val gen = Gen.choose(1, 10)
+    val rng1 = RNG.SimpleRNG(10)
+    val (n1, rng2) = gen.sample.run(rng1)
+    val (n2, _) = gen.sample.run(rng1)
+    val (n3, rng3) = gen.sample.run(rng2)
+    assert(n1 == n2)
+    assert(n1 != n3)
   }
 
+  it should "EXERCIZE 8.4 choose2" in {
+    import fpinscala.state._
+    val gen = Gen.choose2(1, 10)
+    val rng1 = RNG.SimpleRNG(10)
+    val (n1, rng2) = gen.sample.run(rng1)
+    val (n2, _) = gen.sample.run(rng1)
+    val (n3, rng3) = gen.sample.run(rng2)
+    assert(n1 == n2)
+    assert(n1 != n3)
+  }
 }
 
