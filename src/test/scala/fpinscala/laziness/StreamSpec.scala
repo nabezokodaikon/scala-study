707c09f90517897654f476a4850c82a8325d1e00
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-29 00:43:00 +0900

    modify キャッシュされるときとされないときのサンプルコードを実装

--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -11,4 +11,30 @@ class StreamSpec extends FlatSpec {
     val h = s.headOption
     assert(h == Some(1))
   }
+
+  it should "キャッシュされない処理のサンプル" in {
+    // println("not cache")が2回出力されることで確認できる。
+    def expensive(x: Int): Int = {
+      println("not cache")
+      x
+    }
+
+    val t = () => Stream(1, 2, 3)
+    val x = Cons(() => expensive(1), t)
+    val h1 = x.headOption
+    val h2 = x.headOption
+  }
+
+  it should "キャッシュされる処理のサンプル" in {
+    // println("cache")が1回出力されることで確認できる。
+    def expensive(x: Int): Int = {
+      println("cache")
+      x
+    }
+
+    val x = Stream(expensive(1), 2, 3)
+    val h1 = x.headOption
+    val h2 = x.headOption
+  }
+
 }
