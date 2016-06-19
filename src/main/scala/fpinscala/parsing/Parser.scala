94b0c91475846b6fa3e9a8e4afa2847eaa00afca
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-06-20 00:11:12 +0900

    add Parserのサンプルファイルを追加

---
+++ src/main/scala/fpinscala/parsing/Parser.scala
@@ -0,0 +1,9 @@
+package fpinscala.parsing
+
+class Parser[A] {
+
+}
+
+object Parser {
+
+}
---
+++ src/test/scala/fpinscala/parsing/ParserSpec.scala
@@ -0,0 +1,12 @@
+package fpinscala.parsing
+
+import org.scalatest.FlatSpec
+
+class ParserSpec extends FlatSpec {
+
+  behavior of "Parser テスト"
+
+  it should "char" in {
+  }
+
+}
